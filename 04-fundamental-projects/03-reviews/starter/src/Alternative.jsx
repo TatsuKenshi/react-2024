acae932cb3b176cc6244692ff8a4c657d9efdcdb
TatsuKenshi <rockinbg@gmail.com>
2024-07-28 01:32:29 +0200

    03 reviews project

--- 04-fundamental-projects/03-reviews/starter/src/Alternative.jsx
+++ 04-fundamental-projects/03-reviews/starter/src/Alternative.jsx
@@ -1,43 +1,39 @@
 import { useState } from "react";
 import people from "./data";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
-
 const App = () => {
   const [index, setIndex] = useState(0);
-  const { image, name, job, text } = people[index];
+  const { name, job, image, text } = people[index];
 
-  const checkNumber = (number) => {
-    if (number > people.length - 1) {
-      return 0;
-    }
-    if (number < 0) {
-      return people.length - 1;
-    }
-    return number;
-  };
+  // 1 % 4 === 1
+  // 2 % 4 === 2
+  // 3 % 4 === 3
+  // 4 % 4 === 0
 
   const nextPerson = () => {
-    setIndex((prev) => {
-      const newIndex = prev + 1;
-      return checkNumber(newIndex);
+    setIndex((index) => {
+      // let newIndex = index + 1;
+      // console.log(newIndex);
+      // console.log(newIndex % people.length);
+      const newValue = (index + 1) % people.length;
+      console.log(newValue);
+      return newValue;
     });
   };
-
   const prevPerson = () => {
-    setIndex((prev) => {
-      const newIndex = prev - 1;
-      return checkNumber(newIndex);
+    setIndex((index) => {
+      const newValue = (index - 1 + people.length) % people.length;
+      return newValue;
     });
   };
-
   const randomPerson = () => {
     let randomNumber = Math.floor(Math.random() * people.length);
-    setIndex(randomNumber);
-
     if (randomNumber === index) {
       randomNumber = index + 1;
     }
-    setIndex(checkNumber(randomNumber));
+    const newIndex = randomNumber % people.length;
+    // console.log(randomNumber, newIndex);
+    setIndex(newIndex);
   };
 
   return (
@@ -49,24 +45,23 @@ const App = () => {
             <FaQuoteRight />
           </span>
         </div>
-        <div>
-          <h4 className="author">{name}</h4>
-          <p className="job">{job}</p>
-          <p className="info">{text}</p>
-        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
         <div className="btn-container">
-          <button type="button" className="prev-btn" onClick={prevPerson}>
+          <button className="prev-btn" onClick={prevPerson}>
             <FaChevronLeft />
           </button>
-          <button type="button" className="next-btn" onClick={nextPerson}>
+          <button className="next-btn" onClick={nextPerson}>
             <FaChevronRight />
           </button>
         </div>
-        <button className="btn btn-block" onClick={randomPerson}>
-          random
+        <button className="btn btn-hipster" onClick={randomPerson}>
+          surprise me
         </button>
       </article>
     </main>
   );
 };
+
 export default App;
