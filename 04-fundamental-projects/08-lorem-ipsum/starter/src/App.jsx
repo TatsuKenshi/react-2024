8d880ed677780a278e6b55cec3ff61ef089983cf
TatsuKenshi <rockinbg@gmail.com>
2024-07-31 22:47:32 +0200

    08 lorem ipsum project

--- 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
+++ 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
@@ -1,4 +1,48 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import text from "./data";
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [paragraphs, setParagraphs] = useState([]);
+
+  const handleChange = (e) => {
+    setCount(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const myCount = parseInt(count);
+    setParagraphs(text.slice(0, myCount));
+    setCount(1);
+  };
+
+  return (
+    <section className="section-center">
+      <h4>Tired of boring old lorem ipsum?</h4>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="number">paragraphs:</label>
+        <input
+          type="number"
+          name="count"
+          id="count"
+          value={count}
+          min={1}
+          step={1}
+          max={8}
+          onChange={handleChange}
+        />
+        <button className="btn" type="submit">
+          submit
+        </button>
+      </form>
+
+      <div className="lorem-text">
+        {paragraphs.map((paragraph) => {
+          const id = nanoid();
+          return <p key={id}>{paragraph}</p>;
+        })}
+      </div>
+    </section>
+  );
 };
 export default App;
