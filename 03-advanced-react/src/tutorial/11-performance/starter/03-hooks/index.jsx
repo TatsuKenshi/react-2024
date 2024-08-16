36d6f009e8673194682e7f293949fc0c6046c5e3
TatsuKenshi <rockinbg@gmail.com>
2024-08-16 21:13:41 +0200

    11 performance - all lessons

--- 03-advanced-react/src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ 03-advanced-react/src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -1,21 +1,41 @@
-import { useState } from 'react';
-import { data } from '../../../../data';
-import List from './List';
+import { useState, useCallback, useMemo } from "react";
+import { data } from "../../../../data";
+import List from "./List";
+import slowFunction from "./slowFunction";
+
 const LowerState = () => {
   const [people, setPeople] = useState(data);
   const [count, setCount] = useState(0);
 
+  const value = useMemo(() => slowFunction(), []);
+  console.log(value);
+
+  const removePerson = useCallback(
+    (id) => {
+      const newPeople = people.filter((person) => person.id !== id);
+      setPeople(newPeople);
+    },
+    [people]
+  );
+
   return (
     <section>
       <button
-        className='btn'
+        className="btn"
         onClick={() => setCount(count + 1)}
-        style={{ marginBottom: '1rem' }}
+        style={{ marginBottom: "1rem" }}
       >
         count {count}
       </button>
-      <List people={people} />
+      <List people={people} removePerson={removePerson} />
     </section>
   );
 };
 export default LowerState;
+
+// React.memo(ComponentName) method returns a memoized component, thus lowering the number of re-renders.
+// It checks whether the props of a component higher on the DOM tree have changed. If they haven't, it will prevent that component's re-render.
+
+// useCallback hook allows us to memoize functions. It takes two arguments: function that's being memoized and a dependency array. useCallback returns a memoized version of the function that only changes if one of the values in the dependency array change.
+
+// useMemo hook is used to memoize a value. It takes two arguments - the function that returns the data we want to memoize and a dependency array. The hook will return the memoized value that will change only if one of the values in the dependency array changes.
--- 03-advanced-react/src/tutorial/11-performance/starter/04-react-18/index.jsx
+++ 03-advanced-react/src/tutorial/11-performance/starter/04-react-18/index.jsx
@@ -1,43 +1,68 @@
-import { useState } from 'react';
+import { useState, useTransition, Suspense, lazy } from "react";
+
+const SlowComponent = lazy(() => import("./SlowComponent"));
+
 const LatestReact = () => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [items, setItems] = useState([]);
+  const [isPending, startTransition] = useTransition();
+  const [show, setShow] = useState(false);
 
   const handleChange = (e) => {
     setText(e.target.value);
 
     // slow down CPU
-    // const newItems = Array.from({ length: 5000 }, (_, index) => {
-    //   return (
-    //     <div key={index}>
-    //       <img src='/vite.svg' alt='' />
-    //     </div>
-    //   );
-    // });
-    // setItems(newItems);
+    startTransition(() => {
+      const newItems = Array.from({ length: 5000 }, (_, index) => {
+        return (
+          <div key={index}>
+            <img src="/vite.svg" alt="" />
+          </div>
+        );
+      });
+      setItems(newItems);
+    });
   };
+
   return (
     <section>
-      <form className='form'>
+      <form className="form">
         <input
-          type='text'
-          className='form-input'
+          type="text"
+          className="form-input"
           value={text}
           onChange={handleChange}
         />
       </form>
       <h4>Items Below</h4>
 
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '1fr 1fr 1fr',
-          marginTop: '2rem',
-        }}
-      >
-        {items}
-      </div>
+      {isPending ? (
+        <h4>Loading...</h4>
+      ) : (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr",
+            marginTop: "2rem",
+          }}
+        >
+          {items}
+        </div>
+      )}
+
+      <button className="btn" onClick={() => setShow(!show)}>
+        TOGGLE
+      </button>
+      {show && (
+        <Suspense fallback={<h4>Loading...</h4>}>
+          <SlowComponent />
+        </Suspense>
+      )}
     </section>
   );
 };
 export default LatestReact;
+
+// useTransition allows us to update the state without blocking the UI.
+
+// React Suspense allows us to manage the loading state of a component. It provides us a way to "suspend" rendering of a component until some data has been fetched, and display a fallback UI in the meantime. This makes it easier to handle async data loading and provide a smooth user experience.
