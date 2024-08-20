f2b2bec4bd23ecae2054a48d5afd85b31baecea4
TatsuKenshi <rockinbg@gmail.com>
2024-08-20 21:52:59 +0200

    05 axios lessons done

---
+++ 05-axios-tutorial/src/axios/interceptors.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const authFetch = axios.create({
+  baseURL: "https://www.course-api.com",
+  headers: {
+    Accept: "application/json",
+  },
+});
+
+authFetch.interceptors.request.use(
+  (request) => {
+    request.headers["Accept"] = "application/json";
+    console.log("request sent");
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+authFetch.interceptors.response.use(
+  (response) => {
+    console.log("got response");
+    return response;
+  },
+  (error) => {
+    console.log(error.response);
+    if (error.response.status === 404) {
+      // do something
+      console.log("Not Found");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default authFetch;
