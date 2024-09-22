f57128d267926f78c076a702fb3fd8765071b7ee
TatsuKenshi <rockinbg@gmail.com>
2024-09-22 15:50:58 +0200

    07 unsplash project complete

--- 07-unsplash-images/starter/src/SearchForm.jsx
+++ 07-unsplash-images/starter/src/SearchForm.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { useGlobalContext } from "./context/context";
+
+const public_key = "4FrN6M6NCX-LEqjNitXf9xRe6NpU_P4TZjVsscO62_E";
+const secret_key = "wR_sjVMasfRi-Y7WHFzKXtdlOjbWTCChnclQclIrcVI";
 
 const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+  const { setSearchTerm } = useGlobalContext();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    console.log(searchValue);
+    setSearchTerm(searchValue);
+  };
+
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="search"
+          placeholder="cat"
+          className="form-input search-input"
+        />
+        <button type="submit" className="btn">
+          searchs
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
